281fdae1486a0514a4be967826a2d7fe70382822
na2kera <nat3mitaro@gmail.com>
2025-04-15 00:43:52 +0900

    current_userの設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,24 @@
 class ApplicationController < ActionController::API
+  include ActionController::HttpAuthentication::Token::ControllerMethods
+
+  before_action :authenticate_user
+
+  private
+
+  def authenticate_user
+    authenticate_with_http_token do |token, options|
+      begin
+        decoded_token = JWT.decode(token, Rails.application.credentials.jwt_secret_key, true, { algorithm: "HS256" })
+        @current_user = User.find_by(uid: decoded_token[0]["sub"])
+      rescue JWT::DecodeError
+        nil
+      end
+    end
+
+    render json: { error: "Unauthorized" }, status: :unauthorized unless @current_user
+  end
+
+  def current_user
+    @current_user
+  end
 end
