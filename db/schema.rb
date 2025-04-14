f2a6d83d5bf50b5df2f63ebd1efdc52b8046468f
na2kera <nat3mitaro@gmail.com>
2025-04-15 00:09:31 +0900

    スキーマ

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_04_14_000001) do
+ActiveRecord::Schema[8.0].define(version: 2025_04_14_140956) do
+  create_table "qr_codes", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "qrcodes", force: :cascade do |t|
     t.integer "user_id", null: false
     t.string "title", null: false
